a347f64cbe6ef72166ec8f8287977d0ad4749983
paddlesteamer <umuter@protonmail.com>
2011-11-07 02:16:39 +0200

    move test

--- main.c
+++ main.c
@@ -4,14 +4,23 @@
 int main()
 {
 	init_wm();
-
+	
 	switch_window();
-	sleep(1);
+	usleep(500000);
+	move_window_to(WM_RIGHT);
+	usleep(500000);
 	switch_window();
-	sleep(1);
+	usleep(500000);
+	move_window_to(WM_DOWN);
+	usleep(500000);
 	switch_window();
-	sleep(1);
+	usleep(500000);
+	move_window_to(WM_LEFT);
+	usleep(500000);
 	switch_window();
+	usleep(500000);
+	move_window_to(WM_UP);
+	usleep(500000);
 
 	return 0;
 }
