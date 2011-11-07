07cceb925a4c45d551e6a590a9f36cced40f83e6
paddlesteamer <umuter@protonmail.com>
2011-11-07 02:17:32 +0200

    move_window_to added

--- wm.h
+++ wm.h
@@ -1,9 +1,15 @@
 #ifndef _WM_H_
 #define _WM_H_
 
+#define WM_RIGHT 0
+#define WM_LEFT 1
+#define WM_UP 2
+#define WM_DOWN 3
+
 int init_wm();
 void list_windows();
 void switch_desktop();
 void switch_window();
+void move_window_to(int direction);
 
 #endif
