7bfdd861cd38ab4d714f56efeec3b0fd29333638
paddlesteamer <umuter@protonmail.com>
2011-12-09 23:43:42 +0200

    some comments added

--- wm.h
+++ wm.h
@@ -6,11 +6,25 @@
 #define WM_UP 2
 #define WM_DOWN 3
 
+/* every program should call
+   this before every program */
 int init_wm();
+
+/* prints names of the windows to
+   stdout */
 void list_windows();
+
+/* switches current desktop */
 void switch_desktop();
+
+/* switches current active window */
 void switch_window();
+
+/* moves current active window to
+   specified direction */
 void move_window_to(int direction);
+
+/* closes current active window */
 void close_window();
 
 #endif
